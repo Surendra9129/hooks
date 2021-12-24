f32d05afc6b5c867de8c2fd350e06a2abba9badd
Surendra9129 <91534930+Surendra9129@users.noreply.github.com>
2021-12-24 18:21:55 +0530

    hook Assignment completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count,setCount]=useState(1);
+
+  const Calculate=(val)=>{
+    if(count<=0){
+      count=0;
+    }
+    setCount(count+val)
+  }
+
+  const Mul=(val)=>{
+    setCount(count*val)
+  }
+  
+  if(count>=20){
+    return <h1>It reaches maxium limit</h1>
+  }
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1 className="red">Number <span className="green">Counter</span> </h1>
+       <hr></hr>
+       <br></br>
+       <h1> {count}</h1>
+       <button onClick={()=>Calculate(1)}>Add</button>
+       <button onClick={()=>Calculate(-1)}>Substract</button>
+       <button onClick={()=>Mul(2)}>Double Value</button>
+       <h4>{count%2==0?"Even Number":"Odd Number"}</h4>
     </div>
   );
 }
